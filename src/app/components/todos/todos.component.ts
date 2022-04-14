787ee1ddfe5157abced1daf80caa021ac3cc34bf
rade901 <rade.jasenovcanin@yahoo.com>
2022-04-15 01:22:37 +0200

    commit

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -9,7 +9,7 @@ import { Todo } from '../../models/Todo';
 export class TodosComponent implements OnInit {
 
  
-todos:any[] = [Todo];
+todos:Todo[];
 
 
   constructor() {}
@@ -18,13 +18,23 @@ todos:any[] = [Todo];
     this.todos = [
       {
         content: 'First todo',
-        completed: false
+        completed: true
       },
       {
         content: 'Second todo',
         completed: false
       }
-    ];
+    ]
   }
 
+  toggleDone (id:number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+       return v;
+  
+    })
+  }
+  deleteTodo (id:number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
 }
