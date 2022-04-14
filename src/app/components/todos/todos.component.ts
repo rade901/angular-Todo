27876ecee843c171211c730f1d2a5cfda2f1162b
rade901 <rade.jasenovcanin@yahoo.com>
2022-04-14 23:21:41 +0200

    commit

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+ 
+todos:any[] = [Todo];
+
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First todo',
+        completed: false
+      },
+      {
+        content: 'Second todo',
+        completed: false
+      }
+    ];
   }
 
 }
